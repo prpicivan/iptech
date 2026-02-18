44c310432f84c4249391397598c7d0292579a7f4
Ivan Prpic <ivanprpic@MacBook-Pro.local>
2026-02-18 22:25:01 +0100

    Fix globals.css path in localized layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { Navbar } from "@/components/ui/Navbar";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
